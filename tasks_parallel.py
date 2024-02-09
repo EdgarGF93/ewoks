24f41dec705971f8ca305a37764aec51c323891c
edgar <edgar.gutierrez-fernandez@esrf.fr>
2024-02-09 20:12:39 +0100

    new workflows

--- tasks_parallel.py
+++ tasks_parallel.py
@@ -1,5 +1,9 @@
 from ewokscore.task import Task
-
+import ewoksxrpd
+import time
+from ewokscore import TaskWithProgress
+import matplotlib.pyplot as plt
+from ewoks import execute_graph, save_graph, convert_graph
 class SplitFrames(
         Task,
         input_names=["h5file"],
@@ -15,3 +19,152 @@ class PrintFrame(
     ):
     def run(self):
         print(self.inputs.frame)
+
+class PrintString(
+    TaskWithProgress,
+    input_names=["string"],
+):
+    def run(self):        
+        if isinstance(self.inputs.string, str):
+            list_strings = self.inputs.string.split(" ")
+        else:
+            list_strings = self.inputs.string
+
+        for s in list_strings:
+            print(s)
+            time.sleep(0.2)
+
+class PlotImage(
+    Task,
+    input_names=["image"],
+):
+     def run(self):
+         plt.imshow(self.inputs.image, vmin=0, vmax=500)
+         plt.colorbar()
+         plt.savefig("fig.png")
+        #  plt.show()
+         
+
+
+class InputNombre(
+    Task,
+    input_names=["input_nombre"],
+    output_names=["ok", "output_nombre"],
+):
+    def run(self):
+        nombre = self.inputs.input_nombre
+        if isinstance(nombre, str):
+            self.outputs.output_nombre = nombre
+            self.outputs.ok = True
+        else:
+            self.outputs.ok = False
+            self.outputs.output_nombre = ""
+
+class PrintHola(
+    Task,
+    input_names=["nombre"],
+):
+    def run(self):
+        print(f"Hola {self.inputs.nombre}")
+
+class PrintAdios(
+    Task,
+    input_names=["nombre"],
+):
+    def run(self):
+        print(f"Adios {self.inputs.nombre}")
+
+
+class Launcher(
+    Task,
+    input_names=["_run"],
+    output_names=["go"],
+):
+    def run(self):
+        if self.inputs._run == True:
+            self.outputs.go = True
+        else:
+            self.outputs.go = False
+
+
+class Wait1s(
+    Task,
+    input_names=["_run"],
+):
+    def run(self):
+        if self.inputs._run == True:
+            time.sleep(1)
+        else:
+            pass
+
+
+class Benchmark(
+    Task,
+    input_names=["start", "stop"],
+    output_names=["benchmark"],
+):
+    def run(self):
+        if self.inputs.start:
+            st = time.perf_counter()
+
+
+
+
+
+
+
+
+
+
+def generate_branch_wait(node_source:dict, id_node_target:str):
+    wait_workflow = {
+    "graph" : {"id" : f"wait_{id_node_target}"},
+    "nodes" : [node_source,
+               {"id" : id_node_target, "task_type" : "class", "task_identifier" : "tasks_parallel.Wait1s"},
+               ],
+    "links" : [{"source" : node_source["id"], "target" : id_node_target, "data_mapping" : [{"source_output" : "go", "target_input" : "_run"}]},
+               ]
+    }
+    return wait_workflow
+
+
+
+
+def generate_node_link_wait(node_source:dict, id_node_target:str):
+    node_target = {
+        "id" : id_node_target, 
+        "task_type" : "class", 
+        "task_identifier" : "tasks_parallel.Wait1s",
+    }
+    link = {
+        "source" : node_source["id"], 
+        "target" : id_node_target, 
+        "data_mapping" : [{"source_output" : "go", "target_input" : "_run"}],
+    }
+    return node_target, link
+
+
+
+
+def generate_wait_branched_graph(nbranches=5):
+    node_launcher = {
+        "id" : "node_launcher", 
+        "task_type" : "class", 
+        "task_identifier" : "tasks_parallel.Launcher",
+    }
+    nodes = [node_launcher]
+    links = []
+    for ind in range(nbranches):
+        node_target, link = generate_node_link_wait(node_source=node_launcher, id_node_target=f"node_wait_{ind}")
+        nodes.append(node_target)
+        links.append(link)
+    id_graph = f"workflow_{nbranches}_branches"
+    graph = {
+        "graph" : {"id" : id_graph},
+        "nodes" : nodes,
+        "links" : links,
+    }
+    return graph
+
+
+
--- workflows.py
+++ workflows.py
@@ -1,13 +1,146 @@
-
+from ewoksxrpd.tasks.background import SubtractBackground
+from tasks_parallel import PlotImage
+import time
 
 node_split_frames = {
     "id" : "node_split_frames",
     "task_type" : "class",
-    "task_identifier" : "task_parallel.SplitFrames",
+    "task_identifier" : "tasks_parallel.SplitFrames",
 }
 
 node_print = {
     "id" : "node_print",
     "task_type" : "class",
-    "task_identifier" : "task_parallel.PrintFrame",
+    "task_identifier" : "tasks_parallel.PrintFrame",
 }
+
+link_1 = {
+    "source" : "node_split_frames",
+    "target" : ""
+}
+
+
+node_print_string = {
+    "id" : "node_print_string",
+    "task_type" : "class",
+    "task_identifier" : "tasks_parallel.PrintString",
+    "default_inputs" : [{"name" : "string", "value" : "hola"}],
+}
+
+graph_print = {
+    "graph" : {"id" : "wfprint"},
+    "nodes" : [node_print_string],
+}
+
+
+graph_bg = {
+    "graph" : {"id" : "bg"},
+    "nodes" : [
+        {"id" : "node1", "task_type" : "class", "task_identifier" : "ewoksxrpd.tasks.background.SubtractBackground"},
+        {"id" : "node2", "task_type" : "class", "task_identifier" : "tasks_parallel.PlotImage"},
+        ],
+    "links" : [
+        {"source" : "node1", "target" : "node2", "data_mapping" : [{"source_output" : "image", "target_input":"image"}]},
+    ]
+}
+
+
+
+graph_bifurcation = {
+    "graph" : {"id": "bif"},
+    "nodes" : [{"id" : "node1", "task_type" : "class", "task_identifier" : "tasks_parallel.InputNombre"},
+               {"id" : "node2", "task_type" : "class", "task_identifier" : "tasks_parallel.PrintHola"},
+               {"id" : "node3", "task_type" : "class", "task_identifier" : "tasks_parallel.PrintAdios"},
+                ],
+    "links" : [{"source" : "node1", "target" : "node2", "data_mapping" : [{"source_output" : "output_nombre", "target_input" : "nombre"}]},
+               {"source" : "node1", "target" : "node3", "data_mapping" : [{"source_output" : "output_nombre", "target_input" : "nombre"}]},
+               ]
+}
+
+
+grahp_wait_parallel = {
+    "graph" : {"id" : "wait"},
+    "nodes" : [{"id" : "node1", "task_type" : "class", "task_identifier" : "tasks_parallel.Launcher"},
+               {"id" : "node2", "task_type" : "class", "task_identifier" : "tasks_parallel.Wait1s"},
+               {"id" : "node3", "task_type" : "class", "task_identifier" : "tasks_parallel.Wait1s"},
+               {"id" : "node4", "task_type" : "class", "task_identifier" : "tasks_parallel.Wait1s"},
+               {"id" : "node5", "task_type" : "class", "task_identifier" : "tasks_parallel.Wait1s"},
+               {"id" : "node6", "task_type" : "class", "task_identifier" : "tasks_parallel.Wait1s"},
+               ],
+    "links" : [{"source" : "node1", "target" : "node2", "data_mapping" : [{"source_output" : "go", "target_input" : "_run"}]},
+               {"source" : "node1", "target" : "node3", "data_mapping" : [{"source_output" : "go", "target_input" : "_run"}]},
+               {"source" : "node1", "target" : "node4", "data_mapping" : [{"source_output" : "go", "target_input" : "_run"}]},
+               {"source" : "node1", "target" : "node5", "data_mapping" : [{"source_output" : "go", "target_input" : "_run"}]},
+               {"source" : "node1", "target" : "node6", "data_mapping" : [{"source_output" : "go", "target_input" : "_run"}]},
+               ]
+}
+
+
+
+
+
+if __name__ == "__main__":
+    from ewoks import execute_graph, save_graph, convert_graph
+    strings = "hola buenas tardes me llamo edgar"
+
+
+    # execute_graph(
+    #     graph=graph_print,
+    #     inputs = [{"name" : "string", "value" : strings}],
+    # )
+
+
+    # DATA_PATH = "/users/edgar1993a/work/ewoks_parallel/rayonix_A1_spitch_0.12_000_0000.edf"
+    # BG_PATH = "/users/edgar1993a/work/ewoks_parallel/rayonix_10s_spitch_0.12_000_0000.edf"
+    # execute_graph(
+    #     graph=graph_bg,
+    #     inputs=[
+    #         {"name" : "image", "value" : DATA_PATH},
+    #         {"name" : "monitor", "value" : 0.0},
+    #         {"name" : "background", "value" : BG_PATH},
+    #         {"name" : "background_monitor", "value" : 1.0},
+    #     ]
+    # )
+    # convert_graph(graph_bg, "workflow_bg.json")
+
+
+    # convert_graph(graph_bifurcation, "workflow_bif.json")
+    # execute_graph(
+    #     graph=graph_bifurcation,
+    #     engine="ppf",
+    #     inputs=[{"name":"input_nombre", "value" : "edgar"}],
+    # )
+    
+    # convert_graph(grahp_wait_parallel, "workflow_wait.json")
+
+    # st = time.perf_counter()
+    # execute_graph(
+    #     graph=grahp_wait_parallel,
+    #     engine="dask",
+    #     inputs=[{"name":"_run", "value" : True}],
+    # )
+    # print(f"Benchmark: {time.perf_counter() - st:.2f} s")
+
+
+    from tasks_parallel import generate_wait_branched_graph
+    import numpy as np
+    import matplotlib.pyplot as plt
+    x = np.logspace(0,3,100)
+    y = []
+    for nb in x:
+        graph = generate_wait_branched_graph(nbranches=int(nb))
+        # convert_graph(graph, f"{graph["graph"]["id"]}.json")
+        st = time.perf_counter()
+        execute_graph(
+            graph=graph,
+            engine="ppf",
+            inputs=[{"name" : "_run", "value" : True}],
+        )
+        b = time.perf_counter() - st
+        y.append(b)
+    plt.plot(x,y, marker='o', ls='--')
+    plt.xlabel("Branches")
+    plt.ylabel("Time (s)")
+    plt.savefig("benchmark.png")
+    
+
